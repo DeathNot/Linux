c539f4c09219eb90755abb75278028616a10c5ec
DeathNot <725509787@qq.com>
2017-12-20 17:40:04 +0800

    竞争条件测试

---
+++ unix环境高级编程/Chapter 8/test5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+static void charatatime(char *);
+
+int main(int argc, char **argv) {
+  pid_t   pid;
+
+  if ((pid = fork()) < 0) {
+    printf("fork error!\n");
+    exit(1);
+  } else if (pid == 0) {
+    charatatime("output from child!\n");
+  } else {
+    charatatime("output from parent\n");
+  }
+
+  return 0;
+}
+
+static void  charatatime(char *str) {
+  char *ptr;
+  int c;
+
+  setbuf(stdout, NULL);  //标准输出设置为不带缓冲
+  for (ptr = str; (c = *ptr++) != 0; ) {
+    putc(c, stdout);
+  }
+}
