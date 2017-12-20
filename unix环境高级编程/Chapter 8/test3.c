39d15293181c155da12e92ea6d5f380aee4a3987
DeathNot <725509787@qq.com>
2017-12-20 10:17:29 +0800

    Chapter 8

---
+++ unix环境高级编程/Chapter 8/test3.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+
+void pre_exit(int status);
+
+int main(int argc, char **argv) {
+
+  pid_t   pid;
+  int     status;
+
+  if ((pid = fork()) < 0) {
+    printf("fork error!\n");
+    exit(1);
+  } else if (pid == 0) {
+    exit(7);
+  }
+
+  if (wait(&status) != pid) {
+    printf("wait error!\n");
+    exit(1);
+  }
+  pre_exit(status);
+
+  if ((pid = fork()) < 0) {
+    printf("fork error!\n");
+    exit(1);
+  } else if (pid == 0) {
+    abort();
+  }
+
+  if (wait(&status) != pid) {
+    printf("wait error!\n");
+    exit(1);
+  }
+  pre_exit(status);
+
+  if ((pid = fork()) < 0) {
+    printf("fork error!\n");
+    exit(1);
+  } else if (pid == 0) {
+    status /= 0;
+  }
+
+  if (wait(&status) != pid) {
+    printf("wait error!\n");
+    exit(1);
+  }
+  pre_exit(status);
+
+  return 0;
+}
+
+void pre_exit(int status) {
+  if (WIFEXITED(status)) {
+    printf("normal termination, exit status = %d\n", WEXITSTATUS(status));
+  } else if (WIFSIGNALED(status)) {
+    printf("abnormal termination, signal number = %d%s\n",
+            WTERMSIG(status),
+#ifdef WCOREDUMP
+                WCOREDUMP(status) ? " (core file generated)" : "");
+#else
+                "");
+#endif
+  } else if (WIFSTOPPED(status)) {
+    printf("child stopped, signal number = %d\n", WSTOPSIG(status));
+  }
+}
